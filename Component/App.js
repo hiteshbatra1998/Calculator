ba3f6bd1fb61a181afb21bcfbd4c475b23c3eb14
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-04-10 10:21:49 +0530

    Add files via upload

---
+++ Component/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import KeyPad from './KeyPad';
+class App extends React.Component{
+    render(){
+        return(<div>
+                <KeyPad />
+            </div>);
+    }
+}
+
+export default App;
